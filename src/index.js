ddaa2849bf1dc660fbfbfb7d372554499b37f590
Anton Arzamastsev <tonich@sbor.net>
2024-04-18 20:28:24 +0300

    update src/index.js: correct style by ESLint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
 export default (element1, element2) => {
-    const last = _.last([element1, element2]);
-    return last;
+  const last = _.last([element1, element2]);
+  return last;
 };
